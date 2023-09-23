7c909661d1aa05ef6ffa4f484e507d649af2821c
Claudio Diaz <diazperez430@gmail.com>
2023-09-23 18:41:36 -0500

    [+] index.html segundo botón agregado

--- JavaScript/project.js
+++ JavaScript/project.js
@@ -1,15 +1,11 @@
 let elementosAcordeon = document.getElementsByClassName("acordeon");
-
-for (let i = 0; i<elementosAcordeon.length;i++){
+    
+    for (let i = 0; i<elementosAcordeon.length;i++){
     elementosAcordeon[i].addEventListener("click", function(){
         this.classList.toggle("active");
-        let panel=this.nextElementSibiling;
+        let panel=this.nextElementSibling;
         if (panel.style.display == "block"){
             panel.style.display = "none";
         }
-        else panel.style.display = "block"
-    }
-        
-    )
-};
+        else {panel.style.display = "block";}})}
 
